43794f8b6fd4dbb6b4b2bc32e599fbcb4086bbc2
PiotrZb <piotr.zborowski2000@gmail.com>
2024-02-29 01:55:40 +0100

    Project setup

---
+++ api/src/test/java/com/piotrzb/RCCarWebApp/RcCarWebAppApplicationTests.java
@@ -0,0 +1,8 @@
+package com.piotrzb.RCCarWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcCarWebAppApplicationTests {
+}
